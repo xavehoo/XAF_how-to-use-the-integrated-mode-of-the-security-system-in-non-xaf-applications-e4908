45626a5bb5fdc6b9ddc9645d1a20c293a8cc3406
AlexanderZhyrkov <alexander.zhyrkov@devexpress.com>
2019-11-05 15:30:48 +0300

    console multitarget example

--- Console/VB/Program.vb
+++ Console/VB/Program.vb
@@ -31,24 +31,26 @@ Namespace ConsoleApplication
             Dim loginObjectSpace As IObjectSpace = objectSpacesProvider.CreateObjectSpace()
             security.Logon(loginObjectSpace)
 
-            Using file As New StreamWriter("result.txt", False)
-                Dim stringBuilder As New StringBuilder()
-                stringBuilder.Append(String.Format("{0} is logged on." & vbLf, userName))
-                stringBuilder.Append("List of the 'Employee' objects:" & vbLf)
-                Using securedObjectSpace As IObjectSpace = objectSpacesProvider.CreateObjectSpace()
-                    For Each employee As Employee In securedObjectSpace.GetObjects(Of Employee)()
-                        stringBuilder.Append("=========================================" & vbLf)
-                        stringBuilder.Append(String.Format("Full name: {0}" & vbLf, employee.FullName))
-                        If security.IsGranted(New PermissionRequest(securedObjectSpace, GetType(Employee), SecurityOperations.Read, employee, "Department")) Then
-                            stringBuilder.Append(String.Format("Department: {0}" & vbLf, employee.Department.Title))
-                        Else
-                            stringBuilder.Append("Department: [Protected content]" & vbLf)
-                        End If
-                    Next employee
-                End Using
-                file.Write(stringBuilder)
-            End Using
-            Console.ReadLine()
+			Using file As New StreamWriter("result.txt", False)
+				Dim stringBuilder As New StringBuilder()
+				stringBuilder.Append(String.Format("{0} is logged on." & vbLf, userName))
+				stringBuilder.Append("List of the 'Employee' objects:" & vbLf)
+				Using securedObjectSpace As IObjectSpace = objectSpacesProvider.CreateObjectSpace()
+					For Each employee As Employee In securedObjectSpace.GetObjects(Of Employee)()
+						stringBuilder.Append("=========================================" & vbLf)
+						stringBuilder.Append(String.Format("Full name: {0}" & vbLf, employee.FullName))
+						If security.CanRead(Of Employee)(employee.Oid, securedObjectSpace, NameOf(Department)) Then
+							stringBuilder.Append(String.Format("Department: {0}" & vbLf, employee.Department.Title))
+						Else
+							stringBuilder.Append("Department: [Protected content]" & vbLf)
+						End If
+					Next employee
+				End Using
+				file.Write(stringBuilder)
+			End Using
+			Console.WriteLine(String.Format("The result.txt file has been created in the {0} directory.", Environment.CurrentDirectory))
+			Console.WriteLine("Press any key to close a the console...")
+			Console.ReadLine()
         End Sub
         Private Shared Sub RegisterEntities()
             XpoTypesInfoHelper.GetXpoTypeInfoSource()
